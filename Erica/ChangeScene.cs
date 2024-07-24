00a116bb7b320aeb844190fdfa4ca36c4f538ac8
Prisha <66759675+PrishaTHE-PRO@users.noreply.github.com>
2024-07-24 11:04:49 -0500

    Update ChangeScene.cs

--- Erica/ChangeScene.cs
+++ Erica/ChangeScene.cs
@@ -6,9 +6,9 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void MoveToScene(int sceneID)
+    public void MoveToScene(int sceneID) // the int from the build settings
     {
-        SceneManager.LoadScene(sceneID);
+        SceneManager.LoadScene(sceneID); // accesses the scene manager to load the scene from build settings 
     }
 
 
